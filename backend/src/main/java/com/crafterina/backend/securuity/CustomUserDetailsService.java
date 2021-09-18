b8e56bc1c1f39354866c19a97dc9772bda11f4cb
Deshan-Chathuranga <deshanc321@gmail.com>
2021-09-18 13:38:30 +0530

    added login and signup services

---
+++ backend/src/main/java/com/crafterina/backend/securuity/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.crafterina.backend.securuity;
+
+import com.crafterina.backend.exception.ResourceNotFoundException;
+import com.crafterina.backend.model.User;
+import com.crafterina.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username : " + username)
+                );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
